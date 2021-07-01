a90aa9164f2bde0247a960e0326322f458f3e8f2
ms <shaheed_manzur@yahoo.com>
2021-07-01 17:20:33 -0400

    db created

---
+++ models/Post.js
@@ -0,0 +1,49 @@
+// model for blog posts
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+
+// create model
+class Post extends Model {}
+
+// initialize
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post',
+  }
+);
+
+// export
+module.exports = Post;
